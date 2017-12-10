5ad724df31bce4582f15c0b2db550c1761b60d7c
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-10 12:18:18 +0530

    demonstrates how iterator works.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const iterable = ['x', 'y', 'z'];
+const iterator = iterable[Symbol.iterator]();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+/*
+The code block below demonstrates how synchronous iteration works.
+iterator.next() {
+	value: 'x',
+	done: false
+}
+
+iterator.next() {
+	value: 'y',
+	done: false
+}
+
+iterator.next() {
+	value: 'z',
+	done: false
+}
+
+iterator.next() {
+	value: undefined,
+	done true
+}*/
