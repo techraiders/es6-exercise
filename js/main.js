02e254fca506257e675bf88cd9b8c6a3103833a1
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-10 21:19:25 +0530

    Implements asyncIterator to iterate over for... await of loop, but code not working currently.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-/*
-Asynchronous iteration works almot like synchronous iteration except that it involves promises.
-The need for iterating through asynchronous data sources brought about asynchronous iteration.
-The code block below can not return its data asynchronously.
-
-const codebeast = {
-	[Symbol.iterator]: () => {
-		const letter = [`c`, `o`, `d`, `e`, `b`, `e`, `a`, `s`, `t`];
-		return {
-			next: () => ({
-				done: letter.length === 0,
-				value: letter.shift()
-			})
-		}
-	}
-};
-
-// We can iterate through the codebeast object using a for... of loop
-
-for (const letter of codebeast) {
-	console.log(letter);
-}
-
-
 
 /*const iterable = ['x', 'y', 'z', undefined];
 const iterator = iterable[Symbol.iterator]();
@@ -52,4 +28,51 @@ iterator.next() {
 iterator.next() {
 	value: undefined,
 	done true
+}*/
+
+/*
+Asynchronous iteration works almot like synchronous iteration except that it involves promises.
+The need for iterating through asynchronous data sources brought about asynchronous iteration.
+The code block below can not return its data asynchronously.
+
+const codebeast = {
+	[Symbol.iterator]: () => {
+		const letter = [`c`, `o`, `d`, `e`, `b`, `e`, `a`, `s`, `t`];
+		return {
+			next: () => ({
+				done: letter.length === 0,
+				value: letter.shift()
+			})
+		}
+	}
+};
+
+// We can iterate through the codebeast object using a for... of loop
+
+for (const letter of codebeast) {
+	console.log(letter);
+}
+*/
+
+
+/* With asynchronous iteration, the concepts iterable, iterator and iteratorResult work a bit differently. In asynchronous iteration, iterables use the method Symbol.asyncIterator. Instead of returning iterator results directly, the next() method of an async iterator returns a promise. In the code clock below let's try to make the codebeast iterable asynchronous: 
+
+-------------BELOW CODE IS NOT WORKING---------
+
+const codebeast = {
+	[Symbol.asyncIterator]: () => {
+		const letter = [`c`, `o`, `d`, `e`, `b`, `e`, `a`, `s`, `t`];
+		return {
+			next: () => Promise.resolve({
+				done: letter.length === 0,
+				value: letter.shift()
+			})
+		}
+	}
+};
+
+// we can carry out our iteration asynchronously using a for... await... of loop:
+
+for (const letter of codebeast) {
+	console.log(letter);
 }*/
