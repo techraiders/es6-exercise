703ee0196463a0aea63402a7a575636c9d345493
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-10 12:40:02 +0530

    Defines iterator object using Symbol.iterator and iterates over it using for... of loop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-const iterable = ['x', 'y', 'z', undefined];
+/*
+Asynchronous iteration works almot like synchronous iteration except that it involves promises.
+The need for iterating through asynchronous data sources brought about asynchronous iteration.
+The code block below can not return its data asynchronously.
+
+const codebeast = {
+	[Symbol.iterator]: () => {
+		const letter = [`c`, `o`, `d`, `e`, `b`, `e`, `a`, `s`, `t`];
+		return {
+			next: () => ({
+				done: letter.length === 0,
+				value: letter.shift()
+			})
+		}
+	}
+};
+
+// We can iterate through the codebeast object using a for... of loop
+
+for (const letter of codebeast) {
+	console.log(letter);
+}
+
+
+
+/*const iterable = ['x', 'y', 'z', undefined];
 const iterator = iterable[Symbol.iterator]();
 
 console.log(iterator.next());
@@ -28,4 +53,3 @@ iterator.next() {
 	value: undefined,
 	done true
 }*/
-
