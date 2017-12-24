c8525fc3dafd5b458c61c37be5cb365f1c8ef14e
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-24 11:50:01 +0530

    Defines our own custom iterator inside/for a JavaScript object.

--- js/main.js
+++ js/main.js
@@ -76,3 +76,37 @@ for (const letter of codebeast) {
 	console.log(letter);
 }
 */
+
+
+
+
+
+
+
+
+
+/* CREATING OUR OWN CUSTOM ITERATOR IN JAVASCRIPT OBJECT*/
+const obj = {
+	[Symbol.iterator] () {
+		let st = this.start, en = this.end, idx = st;
+
+		const it = {
+			next: () => {
+				if (idx <= en){
+					let v = this.values[idx];
+					idx++;
+					return {value: v, done: false};
+				} else {
+					return {done: true};
+				}
+			}
+		};
+		return it;
+	},
+	values: [2,4,6,8,10,12,14,16,18,20,22,24,26,28],
+	start: 4,
+	end: 13
+};
+
+let vals = [...obj];
+console.log(vals);
