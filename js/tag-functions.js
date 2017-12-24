35a1f4afb8c967028057dda8eabc96f43bec76cf
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-24 19:41:22 +0530

    Improves tag function and solves real problem, using strings, and values.

--- js/tag-functions.js
+++ js/tag-functions.js
@@ -1,9 +1,18 @@
 /* Tag function serves kind of like a pre processors, it's like a macro */
 
-function foo (strings, ...values) {
+/*function foo (strings, ...values) {
 	console.log(strings); // ["Hello, ", ", your order (#", ") was $", ".", raw: Array(4)]
 	console.log(values); // ["Kyle", "123", 319.7]
 	return 'I am evil';
+}*/
+
+function foo (strings, ...values) {
+	let str = '';
+	for (let i = 0; i < strings.length; i++) {
+		if (i) str += values[i - 1];
+		str += strings[i];
+	}
+	return str;
 }
 
 let name = 'Kyle';
@@ -13,4 +22,5 @@ let total = 319.7;
 let msg = foo`Hello, ${name}, your \
 order (#${orderNumber}) was $${total}.`;
 
-console.log(msg); // 'I am evil'
+//console.log(msg); // 'I am evil'
+console.log(msg); // 'Hello, Kyle, your order (#123) was $319.7.'
