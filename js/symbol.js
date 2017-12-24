f283450d15b22a741cb8ed85b12cd778b62e72f2
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-24 11:20:00 +0530

    Lists some well-known symbols and their purpose.

--- js/symbol.js
+++ js/symbol.js
@@ -16,7 +16,10 @@ console.log(courseInfo); // {title: "ES6", topics: Array(4), Symbol(): 41284}
 // Symbol() avoids naming conflict, if another developer adds a property 'id' to the courseInfo object as our object remains intact below:
 
 courseInfo.id = 'js_course';
-console.log(courseInfo); // {title: "ES6", topics: Array(4), id: "js_course", Symbol(): 41284}*/
+console.log(courseInfo); // {title: "ES6", topics: Array(4), id: "js_course", Symbol(): 41284}
+
+
+
 
 
 // The symbol function optionally takes the string value, that works as label for debugging.
@@ -42,4 +45,25 @@ obj[xy] = 'This is the un-guessable unique value';
 console.log(obj); // {id: 42, Symbol(Whatever): "This is the un-guessable unique value"}
 console.log(Object.keys(obj)); // ["id"]
 console.log(Object.getOwnPropertyNames(obj)); // ["id"]
-console.log(Object.getOwnPropertySymbols(obj)); // [Symbol(Whatever)]
+console.log(Object.getOwnPropertySymbols(obj)); // [Symbol(Whatever)]*/
+
+
+
+
+/* JavaScript engines ships with some well known symbols, these are some property of Symbol() function object. Following are some special symbols:
+
+  1. Symbol.iterator
+  2. Symbol.toStringTag
+  3. Symbol.toPrimitive
+  4. Symbol.isConcatSpreadable
+
+there are about a dozen or more of these predefines well-known symbols
+
+Purpose:
+  1. Tell the programmer meta hooks for extending build in javascript behavior.
+
+  	e.g If we have any iterable object, we want to customize the iterator, the location on the object to do that is at: Symbol.iterator
+
+  	e.g If we want to hook how things are stringified whenever we call toString() on it I customize that at the hook location toStringTag.
+
+  	e.g I we want to control whether or not an object should be spread if it's passed to Concat method, we control that with isConcatSpreadable.*/
