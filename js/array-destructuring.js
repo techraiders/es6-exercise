8adf36bc329a09114e78ca2ab838d74d9a0fa880
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-25 15:36:38 +0530

    Dumps unwanted values of an array using array destructuring, gather, and spread operator.

--- js/array-destructuring.js
+++ js/array-destructuring.js
@@ -5,7 +5,7 @@ function foo () {
 //var [a,b,c] = foo();
 //console.log(a,b,c); // 1, undefined, undefined
 
-let [a,b = 42, c] = foo();
+// let [a,b = 42, c] = foo();
 //console.log(a,b,c); // 1,42,undefined
 
 /*******************************************/
@@ -16,7 +16,7 @@ function bar () {
 /*let [x,y,z] = bar(); // Invalid attempt to destructure non-iterable instance
 */
 
-let [x,y,z] = bar() || []; // Prevents crash / exception
+//let [x,y,z] = bar() || []; // Prevents crash / exception
 
 /**********************************************/
 
@@ -38,4 +38,20 @@ let l, m, n;
 
 let o = {};
 [o.a, o.b, o.c, ...o.args] = baaz();
-console.log(o); // {a: 1, b: 2, c: 3, args: [4,5,6,7,8,9]}
+//console.log(o); // {a: 1, b: 2, c: 3, args: [4,5,6,7,8,9]}
+
+
+/* SWAPPING VALUE USING DESTRUCTURING */
+//let x = 10, y = 20;
+//[x,y] = [y,x];
+//console.log(x,y); // 20, 10
+
+
+/******************************************/
+
+let a = [1,2,3];
+//[x,y, ...a] = [0,...a,4];
+// console.log(a); // 2,3,4
+
+[ , , ...a] = [0, ...a, 4];
+console.log(a); // 2,3,4
