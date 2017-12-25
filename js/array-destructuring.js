5c084ea36b9e2af4793b8ca754fb5007d51222b6
Navneet Prakash <hello_navneet@hotmail.com>
2017-12-25 15:23:53 +0530

    Demonstrates array-destructuring.

---
+++ js/array-destructuring.js
@@ -0,0 +1,41 @@
+function foo () {
+	return [1];
+}
+
+//var [a,b,c] = foo();
+//console.log(a,b,c); // 1, undefined, undefined
+
+let [a,b = 42, c] = foo();
+//console.log(a,b,c); // 1,42,undefined
+
+/*******************************************/
+
+function bar () {
+  return null;
+}
+/*let [x,y,z] = bar(); // Invalid attempt to destructure non-iterable instance
+*/
+
+let [x,y,z] = bar() || []; // Prevents crash / exception
+
+/**********************************************/
+
+function baaz () {
+	return [1,2,3,4,5,6,7,8,9];
+}
+ 
+let [p,q,r, ...rest] = baaz();
+//console.log(p,q,r,rest); // 1 2 3, [4, 5, 6, 7, 8, 9]
+
+/*******************************************/
+
+let l, m, n;
+[l, m, n] = baaz();
+
+//console.log(l,m,n); // 1,2,3
+
+/********************************************/
+
+let o = {};
+[o.a, o.b, o.c, ...o.args] = baaz();
+console.log(o); // {a: 1, b: 2, c: 3, args: [4,5,6,7,8,9]}
