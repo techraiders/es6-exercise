df7802972c083d719dd812b66c232662b878b168
Navneet Prakash <hello_navneet@hotmail.com>
2018-10-21 20:31:01 +0530

    Update reflect.js

--- js/reflect.js
+++ js/reflect.js
@@ -24,3 +24,17 @@ function TopObj () {
 let person = Reflect.construct(Person, ['Max'], TopObj);
 console.log(person); // {name: 'Max'};
 console.log(person.__proto__ == TopObj.prototype); // true
+
+CASE 3:
+class Person {
+  constructor (name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greet () {
+    console.log(`Hello, I am ${this.name}`);
+  }
+}
+let person = Reflect.construct(Person, ['Max', 27]);
+Reflect.apply(person.greet, person, []); // 'Hello, I am Max'; 
+Reflect.apply(person.greet, {name: 'Anna'}, []); // 'Hello, I am Anna';
